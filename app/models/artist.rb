c0fd8df7dad0839ed1171357c544b262aab4fc3c
vertreesa <Alessandra.Vertrees82@myhunter.cuny.edu>
2019-07-24 09:15:19 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,71 @@
 class Artist
 
   attr_reader :name, :years_experience
+  @@all = []
 
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
   end
 
+  # * `Artist#paintings`
+  #   * Returns an `array` all the paintings by an artist
+  def paintings
+    Painting.all.select do |painting|
+      painting.artist == self
+    end
+  end
+
+  # * `Artist#galleries`
+  #   * Returns an `array` of all the galleries that an artist has paintings in
+  def galleries 
+    self.paintings.map do |painting|
+      painting.gallery
+    end
+  end
+
+  # * `Artist#cities`
+  #   * Return an `array` of all cities that an artist has paintings in
+  def cities
+    self.galleries.map do |gallery|
+      gallery.city
+    end
+  end
+
+  # * `Artist#create_painting`
+  #   * Given the arguments of `title`, `price` and `gallery`, 
+  #   creates a new painting belonging to that artist
+  def create_painting(title,price,gallery)
+    Painting.new(title, price, self, gallery)
+  end
+
+  # * `Artist.all`
+  #   * Returns an `array` of all the artists
+  def self.all
+    @@all
+  end
+
+  # * `Artist.total_experience`
+  #   * Returns an `integer` that is the total years of experience of all artists
+  def self.total_experience
+    self.all.map do |artist|
+      artist.years_experience
+    end.sum
+  end
+
+  def self.paintings_per_year_per_artist
+    paintings_per_year = {}
+    self.all.map do |artist|
+      paintings_per_year[artist] = artist.years_experience / artist.paintings.count
+    end
+    paintings_per_year.sort_by{|key,value| value}
+  end
+  
+  # * `Artist.most_prolific`
+  #   * Returns an `instance` of the artist with the highest amount of 
+  #   paintings per year of experience.  
+  def self.most_prolific
+    self.paintings_per_year_per_artist.last[0]
+  end
 end
--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -2,9 +2,49 @@ class Gallery
 
   attr_reader :name, :city
 
+  @@all = []
+
   def initialize(name, city)
     @name = name
     @city = city
+    @@all << self
+  end
+
+  # * `Gallery#paintings`
+  #   * Returns an `array` of all paintings in a gallery
+  def paintings
+    Painting.all.select do |painting|
+      painting.gallery == self
+    end
+  end
+
+  # * `Gallery#artists`
+  #   * Returns an `array` of all artists that have a painting in a gallery
+  def artists
+    self.paintings.map do |painting|
+      painting.artist
+    end
+  end
+
+
+  # * `Gallery#artist_names`
+  #   * Returns an `array` of the names of all artists that have a painting in a gallery
+  def artist_names
+    self.artists.map do |artist|
+      artist.name
+    end
+  end
+
+  # * `Gallery#most_expensive_painting`
+  #   * Returns an `instance` of the most expensive painting in a gallery
+  def most_expensive_painting
+    self.paintings.sort_by{|painting| painting.price}.last
+  end
+
+  # * `Gallery.all`
+  #   * Returns an `array` of all the galleries
+  def self.all
+    @@all
   end
 
 end
